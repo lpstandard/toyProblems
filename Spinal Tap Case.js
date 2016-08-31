d6510e9de186630875b76b63ed5a5f9e009f20ce
Tony Kung <lps.tony@gmail.com>
2016-08-31 11:24:34 -0500

    spinal_tap_case

---
+++ Spinal Tap Case.js
@@ -0,0 +1,15 @@
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  var regex = /\s+|_+/g; 
+
+  str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  str = str.replace(regex, '-').toLowerCase();
+
+  return str;
+}
+
+console.log(spinalCase('The_Andy_Griffith_Show'));
