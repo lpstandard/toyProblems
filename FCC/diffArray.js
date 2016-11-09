1e1cf0d820f08a6f0c21b934c73e9d803203c5bc
Tony Kung <lps.tony@gmail.com>
2016-11-09 12:59:38 -0600

    diffArrays.js

---
+++ FCC/diffArray.js
@@ -0,0 +1,12 @@
+function diffArray(arr1, arr2) {
+  var arr = arr1.concat(arr2);
+  console.log(arr)
+  return arr.filter(function(item, i){
+    if(arr.slice(i+1).indexOf(item) === -1 && arr.slice(0, i).indexOf(item) === -1){
+      return item; 
+    }
+  });
+}
+
+ 
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])) // [4]
