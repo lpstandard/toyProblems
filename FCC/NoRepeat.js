7a6ffbdbfd2e21bda7e948bdeb6869b825262702
Tony Kung <lps.tony@gmail.com>
2017-02-14 18:05:02 -0600

    FCC_NoRepeat

--- FCC/NoRepeat.js
+++ FCC/NoRepeat.js
@@ -1,50 +1,35 @@
-// Return the number of total permutations of the provided string that 
-// don't have repeated consecutive letters. Assume that all characters 
-// in the provided string are each unique. For example, aab should return
-// 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), 
-// but only 2 of them (aba and aba) don't have the same letter (in this case a)
-// repeating.
-// permAlone = (str) => {
-//   return 
-// }
+/*Return the number of total permutations of the provided string that 
+don't have repeated consecutive letters. Assume that all characters 
+in the provided string are each unique. For example, aab should return
+2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), 
+but only 2 of them (aba and aba) don't have the same letter (in this case a)
+repeating.
+*/
 
-// permLength = (str) => {
-//   let perm;
-//   if(str.length <= 3){
-//     return str.length * (str.length - 1);
-//   } 
-//   if(str.length > 3){
-//     return permLength(str)
-//   }
+function permAlone(str) {
+  var arr = str.split('');
+  var result = 0;
   
-// }
-// Heap's Algorithm
-generate = (n, array) => {
-  if(n === 1) {
-    return array
-  } else {
-    for(let i = 0; i < n - 1; i++){
-      generate(n - 1, array);
-      if(n % 2 === 0){
-        array[i] = array[n - 1];
-      } else {
-        array[0] = array[n - 1];
+  function swap(a, b) {
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+  
+  function generator(n) {
+    var regex = /([a-z])\1+/;  
+    if(n === 1 && !regex.test(arr.join(''))){
+      result ++;
+    } else {
+      for(var i = 0; i !== n; i++){
+        generator(n - 1);
+        swap(n % 2 ? 0 : i, n - 1);
       }
     }
   }
-  generate(n - 1, array);
+  generator(arr.length);
+  return result; 
 }
 
-// console.log(permAlone("aab")) //2
-// console.log(permAlone("aaa")) //0
-// console.log(permAlone("aabb")) //2
-// console.log(permAlone("abcdefa")) //3600
-// console.log(permAlone("abfdefa")) //2640
-// console.log(permAlone("zzzzzzzz")) //0
-// console.log(permAlone("a")) //1
-// console.log(permAlone("aaab")) //0
-// console.log(permAlone("aaabb")) //12
-let ans = generate(3, ['a', 'a', 'b']);
-console.log(ans)
-
+console.log(permAlone('aab'));
 
