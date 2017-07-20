e653f28a5a03b3c2dda59b353034c123cf9247a4
Tony Kung <lps.tony@gmail.com>
2017-07-20 11:23:21 -0500

    makeArrayConsecutive.js

--- codeFights/makeArrayConsecutive.js
+++ codeFights/makeArrayConsecutive.js
@@ -5,6 +5,8 @@ function makeArrayConsecutive2(statues) {
     let max = statues[statues.length - 1];
     console.log(statues);
     for(let i = min; i < max; i++){
+    // iterate over the sorted array; if a value can't be found
+    // count will be increased by one. 
         if(statues.indexOf(i) === -1){
             count++;
         }    
