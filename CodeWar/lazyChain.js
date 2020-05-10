f7f022b61f0ca4e4e910d9954d6dc28a8588ba5c
Tony Kung <lps.tony@gmail.com>
2020-05-10 16:45:41 +0800

    lazychain

---
+++ CodeWar/lazyChain.js
@@ -0,0 +1,36 @@
+function lazyChain(arg) {
+    // Insert Code Here
+    let invocations = [];
+
+    let chain = {
+        invoke,
+        value
+    }
+
+    function value() {
+        return invocations.reduce((finalValue, invocation) => {
+            return finalValue[invocation.name](...invocation.args);
+        }, arg)
+    }
+
+    function invoke(name, ...args) {
+        invocations.push({ name, args })
+        return chain;
+    }
+
+    return chain;
+}
+
+console.log(
+    lazyChain("hello")
+        .invoke('toUpperCase')
+        .invoke('split', "")
+        .value(), ['H', 'E', 'L', 'L', 'O']
+)
+
+console.log(
+    lazyChain([1, 2, 3])
+        .invoke('map', x => x * x)
+        .invoke('reverse')
+        .value(), [9, 4, 1]
+)
